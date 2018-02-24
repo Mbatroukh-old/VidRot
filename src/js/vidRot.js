86b53c2ee6caaa93805532f91f2aecde20ee8892
Mohammad <mohammad.batroukh@soulfx.com>
2018-02-24 00:48:20 -0500

    :construction: Still working on the initial setup

--- src/js/vidRot.js
+++ src/js/vidRot.js
@@ -1,7 +1,25 @@
-$("#vidRot-range").roundSlider({
-	min: 0,
-	max: 360,
+var vidRot = $("#vidRot-range").roundSlider({
+  radius: 75,
+  width: 20,
+  max: 360,
+  mouseScrollAction: true,
+  startAngle: 270,
+  handleSize: "+16",
+  handleShape: "dot",
+  sliderType: "min-range",
+  tooltipFormat: "tooltipVal2",
+  drag: function (e) {
+	    console.log(e.value);
+	}
 });
+
+function tooltipVal2(args) {
+    return args.value + "°";
+}
+
+function getDeg() {
+	console.log(args.value);
+}
 
 if ($('.html5-video-player').length > 0) {
 	console.log('Yup');
